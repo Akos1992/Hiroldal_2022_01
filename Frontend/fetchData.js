818076f993c66c92439d622c7dc5c4c729311aab
ThomasKenyeres <kenyerest97@gmail.com>
2022-11-30 22:08:08 +0100

    Hírek szűrt megjelenítése a listában

--- Frontend/fetchData.js
+++ Frontend/fetchData.js
@@ -4,12 +4,10 @@ function fetchCategories() {
     fetch(`${backendURL}/news/categories`)
     .then(response => {
         response.json().then(jsonData => {
-            console.log(jsonData);
-            
             for (const category of jsonData) {
                 var categoryHTML = `\
                     <li class="nav-item active"> \
-                        <a class="nav-link" href="#">${category.katNev}</a> \
+                        <a class="nav-link" href="#/kat/${category.katID}">${category.katNev}</a> \
                     </li>`;
                 document.getElementById('categories').innerHTML += categoryHTML;
             }
@@ -20,4 +18,26 @@ function fetchCategories() {
         alert('Hiba a kategóriák lekérésekor')
         console.log(err);
     })
+}
+
+function fetchNewsByCategory(categoryId) {
+    fetch(`${backendURL}/news/categories/${categoryId}`)
+        .then(response => {
+            response.json().then(news => {
+                document.querySelector('#main').innerHTML = 'HTML';
+                for (const newsItem of news) {
+                    var newsItemHTML = `<div class="col-sm">
+                        <a href="" style="text-decoration: none; width: 18rem;" class="card">
+                            <img class="card-img-top" src="" alt="Card image cap">
+                            <div class="card-body">
+                                <h5 class="card-title">${newsItem.cikkCim}</h5>
+                                <p class="card-text">${newsItem.cikkLead}</p>
+                            </div>
+                        </a>
+                    </div>`;
+                    document.querySelector('#main').innerHTML = newsItemHTML;
+                }
+                
+            });
+        })
 }
